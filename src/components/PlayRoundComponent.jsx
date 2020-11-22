58e9ee6b8cfdb79263cf8db7fd057023687d08a9
Miguel Alcantara <miguel.alc@gmail.com>
2020-11-22 18:11:40 -0500

    last table

--- src/components/PlayRoundComponent.jsx
+++ src/components/PlayRoundComponent.jsx
@@ -16,24 +16,15 @@ export default class PlayRoundComponent extends React.Component {
             totalRoundsPlayed: '',
             totalWinsFirstPlayer: '',
             totalWinsSecondPlayer: '',
-            totalDraws: ''
+            totalDraws: '',
+            firstPlayerChoose: '',
+            secondPlayerChoose: 'Rock',
+            roundResult: '',
+            roundUsername: '',
         };
 
         this.playGameChange = this.playGameChange.bind(this);
         this.playGameSubmit = this.playGameSubmit.bind(this);
-    }
-
-    componentDidMount() {
-        // axios.get(`http://localhost:8080/match/resume`)
-        // .then(resume => resume.data)
-        // .then((resumeData) => {
-        //     console.log('===>', resumeData)
-        //     this.setState({
-        //         totalRoundsPlayed: resumeData.numberRoundsPlayed,
-        //         totalWinsFirstPlayer: resumeData.winsFirstPlayer,
-        //         totalWinsSecondPlayer: resumeData.winsSecondPlayer,
-        //     })
-        // }).catch(console.log('vata mierda'))
     }
     
     playGameSubmit(event) {
@@ -58,7 +49,7 @@ export default class PlayRoundComponent extends React.Component {
                 totalWinsSecondPlayer: resumeData.winsSecondPlayer,
                 totalDraws: resumeData.totalDraws
             })
-        }).catch(console.log('vata mierda'))
+        }).catch(console.log('cannot get resume'))
 
         // players move
         axios.get(`http://localhost:8080/match/players`)
@@ -66,6 +57,18 @@ export default class PlayRoundComponent extends React.Component {
         .then((playersResumeData) => {
 
             console.log('------>>>>', playersResumeData)
+            for(var foo in playersResumeData) {
+                console.log('foo: ', foo, ', bar: ', playersResumeData[foo].gameDetail[0].firstPlayerSelection);
+                for(var bar in playersResumeData[foo].gameDetail) {
+                    console.log('elliot: ', playersResumeData[foo].gameDetail[bar].firstPlayerSelection);
+                    this.setState({
+                        roundUsername: foo,
+                        firstPlayerChoose: playersResumeData[foo].gameDetail[bar].firstPlayerSelection,
+                        roundResult: playersResumeData[foo].gameDetail[bar].matchResult,
+                    })
+
+                }
+            }
         })
         
         event.preventDefault();
@@ -81,35 +84,35 @@ export default class PlayRoundComponent extends React.Component {
 
     render() {
         return (<>
-           
-            
             <form autoComplete="off" onSubmit={this.playGameSubmit}>
                 <div>
                     <label>
                         Name: <input type="text" value={this.state.userName} onChange={this.playGameChange} autoFocus={true}/>
                     </label>
-                    <input type="submit" value="Play" />
+                    <input type="submit" value="Play Round" />
                 </div>
             </form>
 
 
-            <h1>Current user stats</h1>    
+            <h1>Current User stats</h1>    
             <Table striped={true} bordered={true} hover>
                 <thead>
                     <tr>
                         <th>User Name</th>
                         <th>Rounds</th>
+                        <th>Reset stats</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
                         <td>{this.state.userNameResponse}</td>
                         <td>{this.state.userRounds}</td>
+                        <td><button onClick={this.handleClick.bind(this, this.state.userNameResponse)}>Reset</button></td>
                     </tr>
                 </tbody>
             </Table>
 
-            <h1>Second Part</h1>
+            <h1>Game stats</h1>
             <Table>
                 <thead>
                     <tr>
@@ -129,7 +132,39 @@ export default class PlayRoundComponent extends React.Component {
                 </tbody>
             </Table>
 
+            <h1>Rounds played</h1>    
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Player name</th>
+                        <th>First player choose</th>
+                        <th>Second player chooose</th>
+                        <th>Round resul</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td>{this.state.roundUsername}</td>
+                        <td>{this.state.firstPlayerChoose}</td>
+                        <td>{this.state.secondPlayerChoose}</td>
+                        <td>{this.state.roundResult}</td>
+                    </tr>
+                </tbody>
+            </Table>
             </>
         )
+    }
+
+    handleClick(user, e) {
+        e.preventDefault();
+        console.log('prevent default: ', user);
+        
+        axios.patch(`http://localhost:8080/match/player/${user}/restart`)
+        .then(res => res.data)
+        .then((data) => {
+            console.log('UPDATE MAS NA')
+        }).catch(this.setState({
+            responseUser: 'WRONG'
+        }))
     }
 }
